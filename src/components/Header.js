ad4775091602cca8492629a50149e4bfebeb1f57
Beacah <beacmrdcm@gmail.com>
2021-06-28 21:17:01 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export default () => {
+    return (
+        <header>
+            <div className = "header--logo">
+                <a href= "/">
+                    
+                </a>
+            </div>
+        </header>
+    );
+}
